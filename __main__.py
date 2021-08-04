a427d843a8bfbd0a7720020cb1753f83f6284aba
Ted Tibbetts <ted@circulated.ca>
2021-08-04 09:22:41 -0300

    One-line summary available via moracle.

--- __main__.py
+++ __main__.py
@@ -2,6 +2,19 @@ from lxml import etree
 import sys
 import argparse
 import sf_price_fetcher
+import moracle
+
+debug = lambda *a: None
+debug = print
+
+identity = lambda x: x
+class moracle_lookup:
+    db = None
+    @classmethod
+    def fullname(cls, card_name):
+        if cls.db is None:
+            cls.db = moracle.load_db()
+        return moracle.format_oneline(moracle.lookup_full(cls.db, card_name))
 
 def convert_card(card):
     return f"{card.attrib['number']}x {card.attrib['name']}"
@@ -9,13 +22,19 @@ def convert_card(card):
 def print_list(mainboard, **kwargs):
     print('\n'.join(convert_card(card) for card in mainboard))
 
-def print_pricing(mainboard, order='list'):
+def print_pricing(mainboard, order='list', render_name=identity):
     total_price = 0
     data = []
     for card in mainboard:
         count = card.attrib['number']
         name = card.attrib['name']
         price = sf_price_fetcher.fetcher.get(name)
+
+        try:
+            name = render_name(name)
+        except TypeError as e:
+            debug(f'moracle error for card name {name}: {e}')
+
         if order == 'list': # if we're printing in list order, do so as prices are fetched
             print(f'{count}x {name} @${price}')
         else:
@@ -34,7 +53,10 @@ argparser.add_argument('-p', '--prices', dest='action', action='store_const',
                        const=print_pricing, default=print_list,
                        help='Fetch and print pricing data as well as total deck price')
 argparser.add_argument('-o', '--order', choices=['list', 'name', 'price'],
-                       default='list')
+                       default='list', help='Sort the card list by name or price')
+argparser.add_argument('-s', '--summary', dest='render_name', action='store_const',
+                       const=moracle_lookup.fullname, default=identity,
+                       help='Concisely summarize each card')
 argparser.add_argument('filename', type=str)
 args = argparser.parse_args()
 
@@ -45,4 +67,4 @@ with open(args.filename, 'r') as cod_file:
 
 mainboard = tree.xpath("//zone[@name = 'main']/card")
 
-args.action(mainboard, order=args.order)
+args.action(mainboard, order=args.order, render_name=args.render_name)
