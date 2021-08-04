7b643d3e532fcb6308e122781696c27ca5a8e794
Ted Tibbetts <ted@circulated.ca>
2021-08-04 08:20:13 -0300

    Added sorting by name and price.

--- __main__.py
+++ __main__.py
@@ -6,23 +6,35 @@ import sf_price_fetcher
 def convert_card(card):
     return f"{card.attrib['number']}x {card.attrib['name']}"
 
-def print_list(mainboard):
+def print_list(mainboard, **kwargs):
     print('\n'.join(convert_card(card) for card in mainboard))
 
-def print_pricing(mainboard):
+def print_pricing(mainboard, order='list'):
     total_price = 0
+    data = []
     for card in mainboard:
         count = card.attrib['number']
         name = card.attrib['name']
         price = sf_price_fetcher.fetcher.get(name)
-        print(f'{count}x {name} @${price}')
+        if order == 'list': # if we're printing in list order, do so as prices are fetched
+            print(f'{count}x {name} @${price}')
+        else:
+            data.append({'count': count, 'name': name, 'price': price})
         total_price += int(count) * float(price)
+
+    if order != 'list':
+        data = sorted(data, key=lambda i: i[order])
+        datavals = (i.values() for i in data)
+        print('\n'.join(f'{count}x {name} @${price}' for count, name, price in datavals))
+
     print(f'total price: ${round(total_price, ndigits=2)}')
 
 argparser = argparse.ArgumentParser(description='Cockatrice COD file converter.')
 argparser.add_argument('-p', '--prices', dest='action', action='store_const',
                        const=print_pricing, default=print_list,
                        help='Fetch and print pricing data as well as total deck price')
+argparser.add_argument('-o', '--order', choices=['list', 'name', 'price'],
+                       default='list')
 argparser.add_argument('filename', type=str)
 args = argparser.parse_args()
 
@@ -33,4 +45,4 @@ with open(args.filename, 'r') as cod_file:
 
 mainboard = tree.xpath("//zone[@name = 'main']/card")
 
-args.action(mainboard)
+args.action(mainboard, order=args.order)
